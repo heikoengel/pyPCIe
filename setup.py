ccd08ef9e9211dd3eae23f85cbdc567a79bf197c
Heiko Engel <heikoengel@users.noreply.github.com>
2021-10-19 20:13:41 +0200

    setup: description & intendation

--- setup.py
+++ setup.py
@@ -2,22 +2,26 @@ from setuptools import setup, find_packages
 
 VERSION = '0.0.1'
 DESCRIPTION = 'Simple Python module to access PCIe devices'
-LONG_DESCRIPTION = ""
+with open("README.md", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
 
 # Setting up
 setup(
-        name="PyCIe",
-        version=VERSION,
-        author="Heiko Engel",
-        author_email="<heikoengel@users.noreply.github.com>",
-        description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
-        packages=find_packages(),
-        install_requires=[],
-        keywords=['python', 'pcie'],
-        classifiers= [
-            "Development Status :: 3 - Alpha",
-            "Programming Language :: Python :: 3",
-            "Operating System :: POSIX :: Linux",
-        ]
+    name="PyCIe",
+    version=VERSION,
+    author="Heiko Engel",
+    author_email="<heikoengel@users.noreply.github.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    url="https://github.com/heikoengel/PyCIe",
+    install_requires=[],
+    keywords=['python', 'pcie'],
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ]
 )
