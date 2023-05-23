2a0caf4aa9349f7d8ff694ac76332034af802e64
Heiko Engel <heikoengel@users.noreply.github.com>
2023-05-23 16:34:22 +0200

    bump version to 0.1.1 for pypi

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 DESCRIPTION = 'Simple Python module to access PCIe devices'
 with open("README.md", "r", encoding="utf-8") as fh:
     LONG_DESCRIPTION = fh.read()
